83782a86fff5e8e37cd6caba118d4eb7e680dc73
Bickolus <bickoblicko@gmail.com>
2025-10-20 01:16:08 -0400

    Added the newproject file and made an attempt to fix Q3's command

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,10 +37,10 @@ cd ..
 mv rawdata data/raw
 
 # 3. List the contents of the ./data/raw directory
-cd data
-ls raw
+ls data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+cd data
 mkdir processed
 cd processed
 mkdir server_logs
